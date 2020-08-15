6d542fdccae2c41ad3b838f9741fb23bedff664d
Suyank Saraswat <31132741+suyanksaraswat@users.noreply.github.com>
2020-08-16 05:05:55 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-	apiKey: "AIzaSyBigkCJ7yFPOAQch1PvKayYuB2JRSBNTiE",
-	authDomain: "instagram-clone-ef527.firebaseapp.com",
-	databaseURL: "https://instagram-clone-ef527.firebaseio.com",
-	projectId: "instagram-clone-ef527",
-	storageBucket: "instagram-clone-ef527.appspot.com",
-	messagingSenderId: "1095308979660",
-	appId: "1:1095308979660:web:71d0548d8628b28965c169",
-	measurementId: "G-TEHM1QB1CR"
+	//Add Config here...
 });
 
 const db = firebaseApp.firestore();
